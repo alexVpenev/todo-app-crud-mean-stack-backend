e5ba1c3529ef6fa401ab0b81b5e4118d46d14dd4
alexVpenev <alexVpenev+github@proton.me>
2023-10-04 21:02:26 +0200

    initialized project and wrote README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # todo-app-crud-mean-stack-backend
+
+This is my first project using the MEAN stack (MongoDB, Express, Angular, Node).
+It is a simple web application for a todo list with simple CRUD operations.
+
+The main goal of this project is to deploy a tested and secure application written in the best coding practices and following standards.
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+app.use( function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
+
+app.use(express.json());
+app.listen(3000, () => console.log('Running on port 3000'));
+
+
+app.get('/', (req,res) => {
+    res.send('Hello There!')
+});
+
+
+// app.get('/', (req,res) => {
+//     res.send(403, 'You dont have the right to be here.')
+// });
+
